76210908c1326ebfd8b182d74a85b43ff6a7cc79
LukasDL <lukashevich.dmitry.e@gmail.com>
2023-04-28 22:31:19 +0500

    Merge4

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class PlayerMoveArtem : MonoBehaviour
+=======
+public class PlayerMoveA: MonoBehaviour
+>>>>>>> Stashed changes
 {
     public int[,] patch;
 }
